36b2e0594c95dc0e1e4337d498b9c3624f1860a2
Joshua Silveous <JoshSilveous@gmail.com>
2025-06-23 18:16:32 -0400

    ported useGameState from non-pwa app

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,1 +1,4 @@
 type PageName = 'welcome' | 'setup' | 'game'
+type Players = { [id: string]: { name: string } }
+type Team = { playerIDs: string[]; side: 'solid' | 'stripe' | null }
+type Teams = { one: Team; two: Team }
---
+++ src/hooks/useGameState.ts
@@ -0,0 +1,109 @@
+import { useState } from 'react'
+
+export default function useGameLogic() {
+	const [players, setPlayers] = useState<Players>({})
+	const [teams, setTeams] = useState<Teams>({
+		one: { playerIDs: [], side: null },
+		two: { playerIDs: [], side: null },
+	})
+	const [curPlayerID, setCurPlayerID] = useState<string>('')
+	const [prevPlayerID, setPrevPlayerID] = useState<string>('')
+	const [nextPlayerID, setNextPlayerID] = useState<string>('')
+	const [curTeam, setCurTeam] = useState<GameController['curTeam']>('one')
+
+	const gotoNextPlayer = () => {
+		// using the current player, determine who the new nextPlayer is
+		const curPlayerIndex = teams[curTeam].playerIDs.indexOf(curPlayerID)
+		const calcedNextPlayerID =
+			teams[curTeam].playerIDs.length - 1 === curPlayerIndex
+				? teams[curTeam].playerIDs[0]
+				: teams[curTeam].playerIDs[curPlayerIndex + 1]
+
+		// set prevPlayerID to current curPlayerID
+		setPrevPlayerID(curPlayerID)
+
+		// set curPlayerID to current nextPlayerID
+		setCurPlayerID(nextPlayerID)
+
+		// set nextPlayerID to the calculated next player ID
+		setNextPlayerID(calcedNextPlayerID)
+
+		// update curTeam
+		setCurTeam((prev) => (prev === 'one' ? 'two' : 'one'))
+	}
+
+	const gotoPrevPlayer = () => {
+		// using the current player, determine who the new prevPlayer is
+		const curPlayerIndex = teams[curTeam].playerIDs.indexOf(curPlayerID)
+		const calcedPrevPlayerID =
+			curPlayerIndex === 0
+				? teams[curTeam].playerIDs.at(-1)!
+				: teams[curTeam].playerIDs[curPlayerIndex - 1]
+
+		// set nextPlayerID to current curPlayerID
+		setNextPlayerID(curPlayerID)
+
+		// set curPlayerID to current prevPlayerID
+		setCurPlayerID(prevPlayerID)
+
+		// set prevPlayerID to the calculated next player
+		setPrevPlayerID(calcedPrevPlayerID)
+
+		// update curTeam
+		setCurTeam((prev) => (prev === 'one' ? 'two' : 'one'))
+	}
+
+	const setTeamSides: GameController['setTeamSides'] = (teamOneSide, teamTwoSide) => {
+		// call setTeams to update info
+		setTeams((prev) => {
+			const clone = structuredClone(prev)
+			clone.one.side = teamOneSide
+			clone.two.side = teamTwoSide
+			return clone
+		})
+	}
+
+	const addNewPlayer: GameController['addNewPlayer'] = (name, team) => {
+		const newPlayerID = Math.random().toString(36).substr(2, 9)
+		setPlayers((prev) => ({ ...prev, [newPlayerID]: { name: name } }))
+		setTeams((prev) => {
+			const clone = structuredClone(prev)
+			clone[team].playerIDs = [...clone[team].playerIDs, newPlayerID]
+			return clone
+		})
+	}
+
+	const controller: GameController = {
+		players,
+		teams,
+		curTeam,
+		setTeamSides,
+		addNewPlayer,
+		setTeams,
+		active: {
+			curPlayerID,
+			prevPlayerID,
+			nextPlayerID,
+			gotoNextPlayer,
+			gotoPrevPlayer,
+		},
+	}
+
+	return controller
+}
+
+export interface GameController {
+	players: Players
+	teams: Teams
+	curTeam: 'one' | 'two'
+	setTeams: React.Dispatch<React.SetStateAction<Teams>>
+	setTeamSides: (teamOneSide: 'stripe' | 'solid', teamTwoSide: 'stripe' | 'solid') => void
+	addNewPlayer: (name: string, team: 'one' | 'two') => void
+	active: {
+		curPlayerID: string | null
+		prevPlayerID: string | null
+		nextPlayerID: string | null
+		gotoNextPlayer: () => void
+		gotoPrevPlayer: () => void
+	}
+}
